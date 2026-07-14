72fa7563921664986968aa589093e107fb90d817
Juraj Vetrák <113345586+JurajVetrak@users.noreply.github.com>
2026-07-14 13:56:14 +0200

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/jurajvetrak/taskmanagement/TaskManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.jurajvetrak.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TaskManagementApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jurajvetrak/taskmanagement/TaskManagementApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jurajvetrak.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
